ffc3cd010466f3f7f9130bb1faff174de5f2ae6d
igorlitvintsev <litvincevi@mail.ru>
2021-02-03 00:10:56 +0300

    18.Dialogs

--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -14,7 +14,7 @@ export function Post(props: MessagePropsType) {
             <img src="https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg" alt=""/>
             {props.message}
             <div>
-                <span>{ props.likesCount } Likes</span>
+                <span>Likes</span> { props.likesCount }
             </div>
         </div>
     )
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export function Profile () {
     return (
-        <div className={s.profile}>
+        <div>
             <img src="https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/beaches--ocean/aerial-view-of-the-beach-shore.jpg" alt=""/>
             <div>
                 ava+description
